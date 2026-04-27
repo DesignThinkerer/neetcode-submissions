5c31d943a49ebbc9d55214eef5e437476dcc1354
Théophile Desmedt <34781168+DesignThinkerer@users.noreply.github.com>
2026-04-27 11:47:43 +0200

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+
+        # divide and conquer approach: we take pairs of lists and merge them
+        # until there is only 1 list left
+        while len(lists) > 1:
+            mergedList = []
+
+            # we want pairs of lists so we use 2 as increment
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                
+                # Merge the pair and append to our temporary list
+                mergedList.append(self.mergeTwoLists(l1, l2))
+            
+            # Update 'lists' with the newly merged larger lists
+            lists = mergedList
+            
+        return lists[0]
+
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Standard algorithm to merge two sorted linked lists
+        dummy = ListNode()
+        tail = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+            
+        # Attach any remaining nodes
+        if l1:
+            tail.next = l1
+        else:
+            tail.next = l2
+            
+        return dummy.next
+    
